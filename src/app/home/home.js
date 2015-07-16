ba97fb73d61fc0e7df398b701b57866be9c8be27
Yuting Wang <yuting.wang3@gmail.com>
2015-07-15 23:48:33 -0500

    created addItem function, set up ngrepeat

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,8 +1,9 @@
-
 angular.module( 'Basket.home', [
   'ui.router',
-  'plusOne'
+  'plusOne',
+  'firebase'
 ])
+
 .config(function config( $stateProvider ) {
   $stateProvider.state( 'home', {
     url: '/home',
@@ -15,8 +16,26 @@ angular.module( 'Basket.home', [
     data:{ pageTitle: 'Home' }
   });
 })
-.controller( 'HomeCtrl', function HomeController( $scope ) {
-})
 
-;
+.controller('HomeCtrl',function HomeController($scope,$location,$firebaseArray){
+  var API_URL = 'https://basketapp.firebaseio.com/basket';
+  var fireRef = new Firebase(API_URL);
 
+  $scope.basket = $firebaseArray(fireRef);
+  $scope.newItem = '';
+  $scope.qty = '';
+
+  $scope.addItem = function(){
+    var newItem = $scope.newItem
+    var qty = $scope.qty;
+    //if the length of item is 0 it will return nothing
+    if (!newItem.length){
+      return;
+    }
+    $scope.basket.$add({
+        name:newItem,
+        qty:qty,
+        done:false
+    });
+  };
+});
